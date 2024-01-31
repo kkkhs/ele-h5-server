5af2b69c3e2c0d8c82e89d1d8bda5131688985ae
khs <khs@123.com>
2024-01-31 23:09:35 +0800

    first commit

---
+++ data/goods_list.js
@@ -0,0 +1,19 @@
+module.exports = () => {
+  return {
+    shopId: 1,
+    data: [
+      {
+        value: 1,
+        label: '单品活动',
+      },
+      {
+        value: 2,
+        label: '满减活动',
+      },
+      {
+        value: 3,
+        label: '热销爆款',
+      }
+    ]
+  }
+}
